340100e861e36dff2e2b3bd2f93318a07ffc7e7c
tan-jia-wen <64011431+tan-jia-wen@users.noreply.github.com>
2020-05-03 16:29:16 +0800

    week 2 practical

--- MainActivity.java
+++ MainActivity.java
@@ -1,9 +1,13 @@
 package sg.edu.np.WhackAMole;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+
 import android.os.Bundle;
 import android.util.Log;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -14,25 +18,91 @@ public class MainActivity extends AppCompatActivity {
         - Feel free to modify the function to suit your program.
     */
 
+    private Button buttonLeft;
+    private Button buttonMiddle;
+    private Button buttonRight;
+    private TextView score;
+    private static int points = 0;
+    private static final String TAG = "Whack-A-Mole!";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.v(TAG, "Finished Pre-Initialisation!");
+        buttonLeft = (Button) findViewById(R.id.buttonLeft);
+        buttonMiddle = (Button) findViewById(R.id.buttonMiddle);
+        buttonRight = (Button) findViewById(R.id.buttonRight);
+        score = (TextView) findViewById(R.id.score);
+
+        score.setText("Score: " + points);
+    }
+
+    public void setNewMole()
+    {
+        Button[] buttons = {buttonLeft, buttonMiddle, buttonRight};
+        Random ran = new Random();
+        int location = ran.nextInt(3);
+        Button s = buttons[location];
+        for (Button b : buttons){
+            if (b == s){
+                b.setText("*");
+            }
+            else{
+                b.setText("O");
+            }
+        }
     }
 
     @Override
     protected void onStart(){
         super.onStart();
-        setNewMole();
         Log.v(TAG, "Starting GUI!");
+        setNewMole();
     }
 
-
-    public void setNewMole()
+    public void OnClickButton(View v)
     {
-        Random ran = new Random();
-        int randomLocation = ran.nextInt(3);
+        Button button = (Button) v;
+        if (check(button) == true){
+            points++;
+            score.setText("Score: " + points);
+            Log.v(TAG, "Hit, score added!");
+        }
+        else{
+            if (points > 0) {
+                points--;
+                score.setText("Score: " + points);
+                Log.v(TAG, "Missed, score added!");
+            } else {
+                points = 0;
+                score.setText("Score: " + points);
+                Log.v(TAG, "Missed, score added!");
+            }
+        }
+        setNewMole();
+    }
+
+    public void text(Button b)
+    {
+        if (b == buttonLeft) {
+            Log.v(TAG, "Button Left clicked!");
+        }
+        if (b == buttonMiddle){
+            Log.v(TAG, "Button Middle clicked!");
+        }
+        if (b == buttonRight){
+            Log.v(TAG, "Button Right clicked!");
+        }
+    }
+
+    public boolean check(Button b)
+    {
+        if (b.getText() == "*"){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
